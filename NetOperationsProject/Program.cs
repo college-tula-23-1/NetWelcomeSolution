6a786dc70c6e779a4c841e3e490b1c6c36a1aa50
maxefimov <efimov.top.academy@gmail.com>
2025-04-22 10:41:57 +0300

    Добавьте файлы проекта.

---
+++ NetOperationsProject/Program.cs
@@ -0,0 +1,119 @@
+﻿bool expression = true;
+
+// if else
+/*
+ * 
+if(expression)
+{}
+else
+{}
+
+if(expression)
+{}
+else if
+{}
+else if
+{}
+else
+{}
+
+
+(expression) ? true_block : false_block
+*/
+
+// while, do while
+/*
+while(expression)
+{
+
+}
+
+do
+{
+
+} while (expression);
+*/
+
+// for, foreach
+/*
+for(int i = 0; i < 10; i++)
+{}
+
+foreach(int i in Enumerable.Range(0, 10))
+{ }
+*/
+
+int key = 1;
+switch(key)
+{
+    case 0:
+        Console.WriteLine("Zero");
+        break;
+    case 1:
+        Console.WriteLine("One");
+        break;
+}
+
+Console.WriteLine(Calc(20, 30, '+'));
+
+int Calc(int a, int b, char sign)
+{
+    //switch (sign)
+    //{
+    //    case '+': return a + b;
+    //    case '-': return a - b;
+    //    case '*': return a * b;
+    //    case '/': return a / b;
+    //    default: return 0;
+    //}
+    int result = sign switch
+    { 
+        '+' => a + b,
+        '-' => a - b,
+        '*' => a * b,
+        '/' => a / b,
+        _ => 0
+    };
+
+    return result;
+}
+
+void OperationExample()
+    {
+        int n = 20;
+        int m = 30;
+
+        /* 
+
+         +, -, *, /, %
+        ++, --
+
+        &, |, <<, >>, ^, ~
+
+        >, <, >=, <=, ==, !=
+
+
+        &/&&, |/||, !
+
+        +=, -=, *=, /=, %=
+        &=, |=, ^=, <<=, >>=
+        a <<= b   <=>   a = a << b
+
+        (type) - explicit
+        implcit
+
+        */
+
+        byte b = (byte)n;
+
+        if (n > 0 || ++m > 0)
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+        Console.WriteLine(m);
+
+        float x = 123.5F;
+        n = (int)x;
+    }
+
+
---
+++ NetWelcomeProject/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NetWelcomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Int32 numberTwo;
+            string str;
+
+            const int MAX = 200;
+
+            number = 123;
+            number = -345;
+            number = 0b0101111;
+            number = 0x12A8E4FD;
+
+            double x = 123.56;
+            x = 123.45e-2;
+            float y = 123.56F;
+            decimal money = 56454.67M;
+
+            char ch = '\x3214';
+            ch = 'A';
+
+            string helloStr = "hello";
+
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello, People!");
+            Console.WriteLine("Hello, All!");
+
+            Console.Write("Number = ");
+            number = int.Parse(Console.ReadLine());
+            //number = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
